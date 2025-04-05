e04e13338eaad6674a57f32f85e455168f358992
Szymi76 <szymonkrupa2@o2.pl>
2025-04-06 00:40:31 +0200

    dodanie konfiguracji dotenv w vite

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
-// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
-const URL = "http://localhost:3000";
+console.log(import.meta.env.VITE_SERVER_DEVELOPMENT_URL);
 
-export const socket = io(URL);
+// "undefined" means the URL will be computed from the `window.location` object
+const SERVER_URL =
+  process.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_SERVER_PRODUCTION_URL
+    : import.meta.env.VITE_SERVER_DEVELOPMENT_URL;
+
+export const socket = io(SERVER_URL);
