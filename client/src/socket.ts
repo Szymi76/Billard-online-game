11d475a1a2aa84820621f2f3851aef0744690bc6
Szymi76 <szymonkrupa2@o2.pl>
2025-04-06 13:52:43 +0200

    korekta socketa

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,11 +1,8 @@
 import { io } from "socket.io-client";
 
-console.log(import.meta.env.VITE_SERVER_DEVELOPMENT_URL);
-
 // "undefined" means the URL will be computed from the `window.location` object
 const SERVER_URL =
-  process.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_SERVER_PRODUCTION_URL
-    : import.meta.env.VITE_SERVER_DEVELOPMENT_URL;
+  import.meta.env.VITE_SERVER_PRODUCTION_URL ??
+  import.meta.env.VITE_SERVER_DEVELOPMENT_URL;
 
 export const socket = io(SERVER_URL);
